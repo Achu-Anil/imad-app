f4e5a6b9edecdd269806e33a99452528df5fa1a7
Achu-Anil <30717520+Achu-Anil@users.noreply.github.com>
2017-08-13 22:20:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,6 +94,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName', function(req,res) {
     //articleNAme == article-one
     //articles[articleName] == {} content object for article-one
+    var articleName = res.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
 
