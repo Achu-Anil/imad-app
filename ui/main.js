f26c21620ef0b046269a9fcc2e5d8e236e303f6e
Achu-Anil <30717520+Achu-Anil@users.noreply.github.com>
2017-08-14 22:28:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,5 +9,5 @@ element.innerHTML = 'New Value';
 var img = document.getElementById('madi');
 
 img.onClick = function() {
-    img.style.marginLEft = '100px';
+    img.style.marginLeft = '100px';
 };
