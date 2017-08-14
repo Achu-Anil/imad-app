29bcff66674053aab91ca88b9505aac9a283d0b8
Achu-Anil <30717520+Achu-Anil@users.noreply.github.com>
2017-08-14 22:28:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,4 +2,12 @@ console.log('Loaded Again!');
 
 //Change the text of the main-text div
 var element = document.getElementById('main-text');
+
 element.innerHTML = 'New Value';
+
+
+var img = document.getElementById('madi');
+
+img.onClick = function() {
+    img.style.marginLEft = '100px';
+};
