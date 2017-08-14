ed4cf2001abfd87a096122729d43e60a7b1363cc
Achu-Anil <30717520+Achu-Anil@users.noreply.github.com>
2017-08-14 22:41:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,10 +7,13 @@ element.innerHTML = 'New Value';
 
 
 var img = document.getElementById('madi');
+//Move an image
+function moveRight() {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 
-img.ondoubleclick = function() {
-    img.style.marginLeft = '300px';
-};
 img.onclick = function() {
+    var interval = setInterval(moveRight,100);
     img.style.marginLeft = '100px';
 };
