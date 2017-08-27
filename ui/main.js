744b5836686fdafd840f646dbb3e29f1bbe1d22e
Achu-Anil <30717520+Achu-Anil@users.noreply.github.com>
2017-08-27 12:17:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,14 +10,18 @@ button.onclick = function() {
     //Capture the response and store it in a variable.
     request.onreadystatechange = function() {
         //Above line is used to detect the state of the request
-        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                // Perfect!
+                var counter = request.responseText;
+                 var span = document.getElementById('count');
+                 span.innerHTML = counter.toString();
+            }
+            // There was a problem with the request.
+            // For example, the response may have a 404 (Not Found)
+            // or 500 (Internal Server Error) response code.
         // Everything is good, the response was received.
-        } else {
-         // Not ready yet.
-        }
+        } 
+        // Not ready yet.
     }
-    //Render the variable in the correct span
-    counter++;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
 };
