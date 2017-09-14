bf17d3a5ce793c5eeb94c60f2911390d65a2c26d
Achu-Anil <30717520+Achu-Anil@users.noreply.github.com>
2017-09-14 21:55:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -36,12 +36,35 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     //Make a request to the server and send the name
-    //Capture a list of names and render it as a list
-    var names = ['name1', 'name2', 'name3', 'name4'];
-    var list = '';
-    for(var i=0; i<names.length; i++){
-        list += '<li>' + names[i] + '</li>';
+    //Create a request object
+    var request = new XMLHttpRequest();
+    
+    
+    //Capture the response and store it in a variable.
+    request.onreadystatechange = function() {
+        //Above line is used to detect the state of the request
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                // Perfect!
+                //Capture a list of names and render it as a list
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i=0; i<names.length; i++){
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+            // There was a problem with the request.
+            // For example, the response may have a 404 (Not Found)
+            // or 500 (Internal Server Error) response code.
+        // Everything is good, the response was received.
+        } 
+        // Not ready yet.
     }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+
+    //Make the request
+    request.open('GET', 'http://achuanilsms.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(null);
 };
