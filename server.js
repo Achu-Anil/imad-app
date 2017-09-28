821ca822c62441b209a9d180cadf43a0165e7b3d
Achu-Anil <30717520+Achu-Anil@users.noreply.github.com>
2017-09-28 09:32:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,212 +1,138 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
-var Pool = require('pg').Pool;
-var crypto = require('crypto');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-
-var config = {
-    user: 'achuanilsms',
-    database:'achuanilsms',
-    host:'db.imad.hasura-app.io',
-    port:'5432',
-    password: process.env.DB_PASSWORD //Use the Environment variable called DB_PASSWORD
+function loadLoginForm () {
+    var loginHtml = `
+        <h3>Login/Register to unlock awesome features</h3>
+        <input type="text" id="username" placeholder="username" />
+        <input type="password" id="password" />
+        <br/><br/>
+        <input type="submit" id="login_btn" value="Login" />
+        <input type="submit" id="register_btn" value="Register" />
+        `;
+    document.getElementById('login_area').innerHTML = loginHtml;
     
+    // Submit username/password to login
+    var submit = document.getElementById('login_btn');
+    submit.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  submit.value = 'Sucess!';
+              } else if (request.status === 403) {
+                  submit.value = 'Invalid credentials. Try again?';
+              } else if (request.status === 500) {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              } else {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              }
+              loadLogin();
+          }  
+          // Not done yet
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/login', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        submit.value = 'Logging in...';
+        
+    };
+    
+    var register = document.getElementById('register_btn');
+    register.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  alert('User created successfully');
+                  register.value = 'Registered!';
+              } else {
+                  alert('Could not register the user');
+                  register.value = 'Register';
+              }
+          }
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/create-user', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        register.value = 'Registering...';
+    
+    };
 }
 
-var app = express();
-app.use(morgan('combined'));
-app.use(bodyParser.json());
-app.use(session({
-    secret: 'someRandomValue',
-    cookie: {maxAge: 1000*60*60*24*30}
-}));
-
-function createTemplate(data) {
-    var title = data.title;
-    var date = data.date;
-    var heading = data.heading;
-    var content = data.content;
-    var htmlTemplate = `
-    <html>
-        <head>
-            <title>
-                ${title}
-            </title>
-            <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            <link href="/ui/style.css" rel="stylesheet" />
-        </head>
-        <body>
-            <div class="container">
-                <div>
-                    <a href='/'>Home</a>
-                </div>
-                <hr/>
-                <h3>
-                    ${heading}
-                </h3>
-                <div>
-                    ${date.toDateString()}
-                </div>
-                <div>
-                    ${content}
-                </div>
-            </div>
-        </body>
-    </html>
+function loadLoggedInUser (username) {
+    var loginArea = document.getElementById('login_area');
+    loginArea.innerHTML = `
+        <h3> Hi <i>${username}</i></h3>
+        <a href="/logout">Logout</a>
     `;
-    return htmlTemplate;
-}                                                                               
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-function hash(input,salt) {
-    //How do we create a hash?
-    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return ['pbkdf2','10000', salt, hashed.toString('hex')].join('$');
 }
 
-app.get('/hash/:input',function(req,res) {
-    var hashedString = hash(req.params.input, 'this-is-a-random-salt');
-    res.send(hashedString);
-});
-
-app.post('/create-user', function(req,res) {
-    //username and password taken as input
-    //{"username":"achuanilsms", "password":"password0"}
-    //JSON
-    var username = req.body.username;
-    var password = req.body.password;
-    var salt = crypto.randomBytes(128).toString('hex');
-    var dbString = hash(password,salt);
-    pool.query('INSERT INTO "user" (username,password) VALUES($1,$2)',[username,dbString], function(err,result) {
-        if(err) {
-            res.status(500).send(err.toString());
-        }
-        else {
-            res.send('User Created Successfully: ' + username);
-        }
-    });
-});
-
-app.post('/login',function(req,res) {
-    var username = req.body.username;
-    var password = req.body.password;
-    pool.query('SELECT * FROM "user" WHERE username=$1',[username], function(err,result) {
-        if(err) {
-            res.status(500).send(err.toString());
-        }
-        else {
-            if(result.rows.length===0) {
-                res.status(403).send('Invalid Username!');
-            } 
-            else {
-                var dbString = result.rows[0].password;
-                var salt = dbString.split('$')[2];
-                var hashedPassword = hash(password,salt); //hashing the password entered with the salt from the original password
-                if(hashedPassword===dbString) {
-                    
-                    //Set a session
-                    req.session.auth = {userId: result.rows[0].id};
-                    //set cookie with session id
-                    //internally on the server side, it maps the session id to an object
-                    //{auth: {userId}}
-                    
-                    res.send('Credentials are correct!');
-                    
-                }
-                else {
-                    res.status(403).send('Invalid Password!');
-                }
+function loadLogin () {
+    // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                loadLoggedInUser(this.responseText);
+            } else {
+                loadLoginForm();
             }
         }
-    });
-});
-
-app.get('/check-login', function(req,res) {
-    if(req.session && req.session.auth && req.session.auth.userId) {
-        res.send('You are logged in:'+req.session.auth.userId.toString());
-    }
-    else {
-        res.send('You are not logged in!');
-    }
-});
-
-app.get('/logout', function(req,res) {
-    delete req.session.auth;
-    res.send('Logged out');
-})
-
-var pool = new Pool(config);  
-app.get('/test-db', function(req, res) {
-    //make a select request
-    //return a response with the result
-    pool.query('SELECT * FROM test', function(err,result) {
-        if(err) {
-            res.status(500).send(err.toString());
-        }
-        else {
-            res.send(JSON.stringify(result.rows));
-        }
-    });
-});
-
-var counter = 0;
-app.get('/counter', function(req,res) {
-    counter++;
-    res.send(counter.toString());
-});
-
-var names = [];
-app.get('/submit-name', function (req,res) { // URL: /submit-name?name=xxxxx
-   //Get the name from the request object
-   var name = req.query.name;
-   
-   names.push(name);
-   //JSON = JavaScript Object Notation
-   res.send(JSON.stringify(names));//TODO
-});
-
-app.get('/articles/:articleName', function(req,res) {
-    //articleNAme == article-one
-    //articles[articleName] == {} content object for article-one
-    var articleName = req.params.articleName;
+    };
     
-    pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName] , function(err,result) {
-        if(err) {
-            res.status(500).send(err.toString());
-        }
-        else {
-            if(result.rows.length === 0) {
-                res.status(404).send('Article not found');
+    request.open('GET', '/check-login', true);
+    request.send(null);
+}
+
+function loadArticles () {
+        // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            var articles = document.getElementById('articles');
+            if (request.status === 200) {
+                var content = '<ul>';
+                var articleData = JSON.parse(this.responseText);
+                for (var i=0; i< articleData.length; i++) {
+                    content += `<li>
+                    <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
+                    (${articleData[i].date.split('T')[0]})</li>`;
+                }
+                content += "</ul>"
+                articles.innerHTML = content;
+            } else {
+                articles.innerHTML('Oops! Could not load all articles!')
             }
-            else {
-                var articleData = result.rows[0];
-                res.send(createTemplate(articleData));
-            }
         }
-    }) 
-});
+    };
+    
+    request.open('GET', '/get-articles', true);
+    request.send(null);
+}
 
-app.get('/ui/style.css', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
 
-app.get('/ui/main.js', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
+// The first thing to do is to check if the user is logged in!
+loadLogin();
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 80;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+// Now this is something that we could have directly done on the server-side using templating too!
+loadArticles();
